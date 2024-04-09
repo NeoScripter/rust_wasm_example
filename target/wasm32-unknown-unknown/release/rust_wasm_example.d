16400006e09a16883f1b681132e824a6d27a5a7f
Ilya <sange0337@gmail.com>
2024-04-09 11:38:07 +0800

    Compliled wasm

--- target/wasm32-unknown-unknown/release/rust_wasm_example.d
+++ target/wasm32-unknown-unknown/release/rust_wasm_example.d
@@ -1,1 +1,1 @@
-C:\Users\Ilya\USERPROFILE1\projects\hello_world\rust_wasm_example\target\wasm32-unknown-unknown\release\rust_wasm_example.wasm: C:\Users\Ilya\USERPROFILE1\projects\hello_world\rust_wasm_example\src\lib.rs
+C:\Users\Ilya\USERPROFILE1\javascript\wasm\rust_wasm_example\target\wasm32-unknown-unknown\release\rust_wasm_example.wasm: C:\Users\Ilya\USERPROFILE1\javascript\wasm\rust_wasm_example\src\lib.rs
